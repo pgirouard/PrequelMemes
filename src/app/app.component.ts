969df9a6ee866b82dba610666e822989b617f8a0
pgirouard <phillipe.girouard@gmail.com>
2017-05-23 15:59:29 -0500

    Initial Mockup
    This is the initial commit for the prequel meme website front-end interface.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Test';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  http: Http;
+  imageResults = [];
+
+  constructor(http: Http){
+    this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    console.log(`User Entered: ${searchTerm.value}`);
+    var apiLink = this.link + searchTerm.value;
+
+    this.http.request(apiLink).subscribe((res: Response) => {
+      this.imageResults = res.json().data;
+      console.log(this.imageResults);
+    });
+  }
 }

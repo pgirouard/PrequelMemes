1595776b73192135e0b8c8f6bbab6795c176a1ed
pgirouard <phillipe.girouard@gmail.com>
2017-05-25 15:51:56 -0500

    Fetch from heroku server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,6 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-@Injectable()
-export class yourService {
-  constructor(public http:Http){}
-  getData() {
-    return this.http.get("imageResults.json")
-        .map((res:Response) => res.json().searchResults);
-  }
-}
-
-let myjsondata;
-
-this.yourService.getData().subscribe((data) => {
-  console.log(data);
-  this.myjsondata = data;
-});
 
 @Component({
   selector: 'app-root',
@@ -45,11 +30,29 @@ export class AppComponent {
   performSearch(searchTerm: HTMLInputElement): void {
     console.log(`User Entered: ${searchTerm.value}`);
     
-    var apiLink = this.link + searchTerm.value;
+    //var apiLink = this.link + searchTerm.value;
 
-    this.http.request(apiLink).subscribe((res: Response) => {
-      this.imageResults = res.json().data;
-      console.log(this.imageResults);
+    window.fetch('https://sleepy-oasis-47541.herokuapp.com/keywords', {
+        method: 'POST',
+        mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            keywords: searchTerm
+        })
+    }).then((response) => {
+        //if(err) console.log(err);
+        console.log(response);
+        // response.json().then( data => {
+        //     //window.searchTerm = data;
+        //     console.log(data);
+        // });
     });
+
+    // this.http.request(apiLink).subscribe((res: Response) => {
+    //   this.imageResults = res.json().data;
+    //   console.log(this.imageResults);
+    // });
   }
 }

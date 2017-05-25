d3942ad97a4bd81fb3275da77f917fbe6cc6bd39
pgirouard <phillipe.girouard@gmail.com>
2017-05-25 13:03:43 -0500

    Added call to local json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class yourService {
+  constructor(public http:Http){}
+  getData() {
+    return this.http.get("imageResults.json")
+        .map((res:Response) => res.json().searchResults);
+  }
+}
+
+let myjsondata;
+
+this.yourService.getData().subscribe((data) => {
+  console.log(data);
+  this.myjsondata = data;
+});
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Test';
   link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
   http: Http;
   imageResults = [];
+  
+  //data;
+  // getLocalFile(){
+  //   return this.http.get('assets/imageResults.json')
+  //       .subscribe(res => this.data = res.json());
+  // }
+  //jsonResponse = this.getLocalFile();
 
   constructor(http: Http){
     this.http = http;
@@ -18,6 +44,7 @@ export class AppComponent {
 
   performSearch(searchTerm: HTMLInputElement): void {
     console.log(`User Entered: ${searchTerm.value}`);
+    
     var apiLink = this.link + searchTerm.value;
 
     this.http.request(apiLink).subscribe((res: Response) => {

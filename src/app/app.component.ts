4961238a3f9fdfbb2bc240961f205f0d117fa8d3
pgirouard <phillipe.girouard@gmail.com>
2017-06-01 11:22:39 -0500

    Implement Prequel Meme API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,9 @@ import 'rxjs/add/operator/map';
 
 export class AppComponent {
   title = 'Test';
-  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  //link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  link = 'https://sleepy-oasis-47541.herokuapp.com/keywords?tags='
+  //anakin&skipAmt=0
   http: Http;
   imageResults = [];
   
@@ -21,11 +23,12 @@ export class AppComponent {
   performSearch(searchTerm: HTMLInputElement): void {
     console.log(`User Entered: ${searchTerm.value}`);
     
-    var apiLink = this.link + searchTerm.value;
+    var apiLink = this.link + searchTerm.value + '&skipAmt=0';
 
     this.http.request(apiLink).subscribe((res: Response) => {
-      this.imageResults = res.json().data;
-      console.log(this.imageResults);
+      //this.imageResults = res.json().data;
+      this.imageResults = res.json();
+      console.log(res.json());
     });
   }
 }
